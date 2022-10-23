1db6764f2c79503b0222abde5c6f27060773662f
soulhn <vldnrlf123@naver.com>
2022-10-23 23:22:36 +0900

    vue start

---
+++ vuepj1/src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <div>
+    <h4 :style="myStyle">헤라 빌</h4>
+    <p>{{ price1 }} 만원</p>
+  </div>
+  <div>
+    <h4>베라 빌</h4>
+    <p>{{ price2 }}만원</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+  data() {
+    return {
+      // 데이터 보관
+      price1: 60,
+      price2: 70,
+      myStyle: "color : blue",
+      products: ["역삼동원룸", "천호동원룸", "마포구원룸"],
+    };
+  },
+  components: {},
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
